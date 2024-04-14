ca2889262efa347ddf1a5607e086c2ca9c8d9fbb
bachir <bachiroran1997@gmail.com>
2024-04-14 15:46:31 +0200

    Hello ProgrammingAdvices

--- OddEvenNumbersApps/Program.cs
+++ OddEvenNumbersApps/Program.cs
@@ -8,7 +8,7 @@ namespace MyConsoleApp
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Programming Advices");
 
             int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, };
             PrintNumber("Numbers",Numbers);
