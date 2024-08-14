f1c586a7e4c43cac29eead0f5abdbb77e4046cab
= <bachiroran1997@gmail.com>
2024-08-14 17:45:41 +0200

    use retyrn syntax

--- OddEvenNumbersApps/Program.cs
+++ OddEvenNumbersApps/Program.cs
@@ -11,27 +11,28 @@ namespace MyConsoleApp
             Console.WriteLine("Hello World");
 
             int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, };
-            PrintNumber("Numbers",Numbers);
-            PrintNumber("EvenNumbers",Numbers.Where(x =>IsEvenNumber(x)));
-            PrintNumber("Numbers",Numbers.Where(x =>IsOddNumber(x)));
+            PrintNumber("Numbers", Numbers);
+            PrintNumber("EvenNumbers", Numbers.Where(x => IsEvenNumber(x)));
+            PrintNumber("Numbers", Numbers.Where(x => IsOddNumber(x)));
             Console.ReadKey();
         }
 
-       static void PrintNumber(string Title, IEnumerable<int> Numbers)
+        static void PrintNumber(string Title, IEnumerable<int> Numbers)
         {
-            Console.WriteLine();   
+            Console.WriteLine();
             Console.Write($"{Title} :[");
-            foreach( int i in Numbers ) { 
-               
-                 Console.Write( $" {i}" );
-            
+            foreach (int i in Numbers) {
+
+                Console.Write($" {i}");
+
             }
 
-              Console.Write("]");
+            Console.Write("]");
             Console.WriteLine();
 
         }
-        static bool IsEvenNumber(int Numbers) => Numbers % 2 == 0;
+        static bool IsEvenNumber(int Numbers)
+            { return Numbers % 2 == 0; }
 
 
         static bool IsOddNumber(int Numbers) => !IsEvenNumber(Numbers);
